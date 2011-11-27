d209117af6f3d696bdcca2c91457eb28ac136aa8
s.shkrabak <s.shkrabak@gmail.com>
2011-11-28 01:14:48 +0200

    Updated index.php

--- php/index.php
+++ php/index.php
@@ -1,4 +1,39 @@
-<?
+<?php
+
+include_once 'controllers/ResourceController.php';
+include_once 'controllers/ResourceControllerFactory.php';
+
+$resourceType = $_GET['resource_type'];
+if (isset($resourceType) && strlen($resourceType) > 0)
+{
+    $controller = ResourceControllerFactory::createController($resourceType);
+    $controller->performAction();
+}
+else
+{
+//    ErrorController::showError(405);
+}
+
+// function getAudioResource() {}
+// 
+// $controller = ControllerFactory::createController($type);
+
+/*
+
+resource_type = [news, images, video, audio, ...]
+action = [get, list, search, ...]
+response_type = [json, xml]
+and other params as url get params
+
+
+/resource/news/get.json
+/resource/images/get.json
+/resource/video/get.json
+/resource/audio/get.json?id=21312312
+/resource/audio/get.json?from=21312&count=14
+/resource/audio/list.json
+/resource/audio/search.json?name=werwe&length=32234&int=34232
+*/
 
 // Model api example
 /*
